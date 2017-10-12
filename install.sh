b4912764fdee122e62d97f3c136aa03c9c62bbf5
Michael Wallio <miwalli@microsoft.com>
2017-10-11 22:40:10 -0700

    Add install file for *nix  - Update vimrc to work with cmd and powershell, requires console    emulator to be configured to identify itself as cmder

---
+++ install.sh
@@ -0,0 +1,9 @@
+cp .vimrc ~/.vimrc
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts
+./install.sh
+
+cd ..
+rm -rf fonts
